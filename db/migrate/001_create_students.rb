e80dc2877e019c61e908e4e4a7ece8ec2a52ffbb
Luke Chen <luke.chen.2010@gmail.com>
2020-06-09 02:27:44 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_students.rb
+++ db/migrate/001_create_students.rb
@@ -1,10 +1,8 @@
-class CreatePosts < ActiveRecord::Migration
+class CreateStudents < ActiveRecord::Migration
   def change
-    create_table :posts do |t|
-      t.string :title
-      t.text :description
- 
-      t.timestamps null: false
+    create_table :students do |t|
+      t.string :first_name
+      t.string :last_name
     end
   end
 end
